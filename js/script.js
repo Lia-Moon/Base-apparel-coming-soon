04e72ad3faa043f5a5b5bc869d842bf208af63b3
Lia-Moon <juliasoaresvieira@hotmail.com>
2023-07-09 19:03:33 -0300

    Pequenas alterações estética

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 /* const emailInputValue = document.getElementById("emailInput").value; */
-const emailInput = document.querySelector(".content__form");
-const formAlert = document.querySelector(".content__form--alert");
+/* const emailInput = document.querySelector(".content__form");
+const formAlert = document.querySelector(".content__form--alert"); */
 
-function mensagemDeAlerta() {
+/* function mensagemDeAlerta() {
     if(!formAlert.classList.contains('display')) {
         formAlert.classList.add('display');
     } else {
         formAlert.classList.remove('display');
     }
-}
+} */
 
 function validacaoEmail(field) {
     usuario = field.value.substring(0, field.value.indexOf("@"));
